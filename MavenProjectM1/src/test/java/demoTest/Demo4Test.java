03e986af6a9413dffb0541d4b1bc05ad8e103ea0
AutomationWithPiyush <automation.with.piyush@gmail.com>
2025-10-15 11:35:42 +0530

    Edge was giving error

--- MavenProjectM1/src/test/java/demoTest/Demo4Test.java
+++ MavenProjectM1/src/test/java/demoTest/Demo4Test.java
@@ -8,6 +8,7 @@ import org.testng.annotations.Test;
 public class Demo4Test {
 	@Test
 	public void case4() throws InterruptedException {
+		System.setProperty("webdriver.edge.driver", "./resources/msedgedriver.exe");
 		WebDriver driver = new EdgeDriver();
 		System.out.println("Demo4Test");
 		Thread.sleep(10000);
--- MavenProjectM1/src/test/java/demoTest/Demo5Test.java
+++ MavenProjectM1/src/test/java/demoTest/Demo5Test.java
@@ -8,6 +8,7 @@ import org.testng.annotations.Test;
 public class Demo5Test {
 	@Test
 	public void case5() throws InterruptedException {
+		System.setProperty("webdriver.edge.driver", "./resources/msedgedriver.exe");
 		WebDriver driver = new EdgeDriver();
 		System.out.println("Demo5Test");
 		Thread.sleep(10000);
